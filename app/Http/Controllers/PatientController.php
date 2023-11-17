53f3de5143af98c11936458b5a7abe8153ed0547
sh1z1a <h4sn44ziz4h@gmail.com>
2023-11-17 13:48:03 +0700

    update controller

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -2,16 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Patient;
 use Illuminate\Http\Request;
 
 class PatientController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    // membuat method index untuk menampilkan data
     public function index()
     {
-        //
+
+        // menampilkan data patients dari database
+        $patients = Patient::all();
+
+        // validasi data pasien
+        if($patients){
+             $data = [
+            'message' => 'Get all resource',
+            'data' => $patients
+        ];
+
+        // mengembalikan kode status 200 (request berhasil)
+        return response()->json($data, 200);
+
+        }
+
+        // buat tampilkan data pasien kosong
+        if($patients->isEmpty()){
+            $data = [
+                "message" => "Data is empty",
+                "data" => []
+            ];
+
+            // mengembalikan kode status 200 
+            return response()->json($data, 200);
+        }
+
+        // tampilkan data jika tidak ada
+        else{
+            $data = [
+                "message" => "Data not found",
+                "data" => []
+            ];
+            return response()->json($data, 404);
+        }
     }
 
     /**
@@ -19,31 +52,105 @@ class PatientController extends Controller
      */
     public function create()
     {
-        //
+        
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Patient $patients)
     {
-        //
+        
+        // validasi data dari data yg ditangkap lalu mau di tambahkan
+        $validateData = $request->validate([
+            'name'=>'required',
+            'phone'=>'numeric|required',
+            'address'=>'required',
+        ]);
+
+        // menggunakan model Patient untuk insert data
+        $patients = Patient::create($validateData);
+
+        if($patients){
+
+            $data = [
+            'message' => 'resources is added succesfully',
+            'data' => $patients,
+
+            ]; 
+
+            // mengembalikan data (json) dan kode 201
+            return response()->json($data, 201);
+        }
+
+        // buat tampilkan data pasien kosong
+        if($patients->isEmpty()){
+
+            $data = [
+                "message" => "Resources is empty",
+                "data" => []
+            ];
+
+            // mengembalikan data (json) dan kode status 200 
+            return response()->json($data, 200);
+        }
+
+        // tampilkan data jika tidak ada
+        else{
+            $data = [
+                "message" => "Resources not found",
+                "data" => []
+            ];
+            return response()->json($data, 404);
+        }
+       
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Request $request, Patient $patients)
     {
-        //
+
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Request $request, Patient $patients)
     {
-        //
+        
+        // menghandle data yang tidak ada
+        if($patients){
+            // menangkap data request
+            $input = [
+                'name' => $request-> name ?? $patients->name,
+                'phone' => $request -> phone ?? $patients->phone,
+                'address' => $request -> address ?? $patients->address,
+            ];
+
+            // temukan resources pasien
+            $patients->find($input);
+
+            // melakukan update data
+            $patients->update($input);
+
+            // tampilkan response
+            $data = [
+                'message' => 'Resources is updated successfully',
+                'data' => $patients
+            ];
+
+            // mengembalikan data (json) dan kode 200
+            return response()-> json($data, 200);
+        }
+        else {
+            $data = [
+                'message' => 'Resources not found'
+            ];
+
+            return response()->json($data, 404);
+        }
     }
 
     /**
